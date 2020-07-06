e2c72ef1f190e3a339979f6ccc7a50d14ec27281
alexis Montealegre <ajmu26@gmail.com>
2020-07-05 22:33:07 -0400

    constuyendo

--- adictos_a_redes2.rb
+++ adictos_a_redes2.rb
@@ -2,3 +2,12 @@ def scan_addicts(array)
     results = []
     n = array.count
     n.times do |i|
+        if array [i] >= 90
+            results.push 'mejorable'
+        else array[i] >= 180
+            results.push 'mal'
+        else
+            results.push 'bien'
+        end
+    end
+    print results
