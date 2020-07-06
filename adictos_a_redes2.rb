4a3369e30b6f02b202ddf1099df5ebb10ad5d2d4
alexis Montealegre <ajmu26@gmail.com>
2020-07-05 22:39:39 -0400

    almost done

--- adictos_a_redes2.rb
+++ adictos_a_redes2.rb
@@ -1,13 +1,16 @@
+array = [120, 50, 600, 30, 90, 10, 200, 0, 500]
 def scan_addicts(array)
     results = []
     n = array.count
     n.times do |i|
         if array [i] >= 90
             results.push 'mejorable'
-        else array[i] >= 180
+        elsif array[i] >= 180
             results.push 'mal'
         else
             results.push 'bien'
         end
     end
     print results
+end
+scan_addicts(array)
